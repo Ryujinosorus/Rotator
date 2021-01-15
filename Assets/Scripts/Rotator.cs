3cbab615f286ecad5a91310f63b3c2176158997b
Ryujinosorus <56671819+Ryujinosorus@users.noreply.github.com>
2021-01-15 22:07:33 +0100

    Add files via upload

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+namespace com.technical.test
+{
+    /// <summary>
+    ///     Rotates a transform with a settable angular speed 
+    ///     for a specific duration
+    /// </summary>
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField]
+        private string _identifier = "The Amazing Rotator";
+        [Header("Rotation parameters")]
+        [SerializeField]
+        private float _timeBeforeStoppingInSeconds = 3;
+        [SerializeField]
+        private bool _shouldReverseRotation = false;
+
+        [SerializeField]
+        private RotationSettings _rotationsSettings = default;
+
+        private float _angleModifier = 1f;
+        private float _timePassed;
+
+        void Awake()
+        {
+            CalculateModifierAccordingToReverse();
+        }
+
+        private void CalculateModifierAccordingToReverse()
+        {
+            _angleModifier = 1f;
+            if (_shouldReverseRotation)
+            {
+                _angleModifier = -1f;
+            }
+        }
+
+        void OnEnable()
+        {
+            _timePassed = 0f;
+        }
+
+        void Update()
+        {
+            Vector3 angularSpeedPerSeconds = _rotationsSettings.AngleRotation / _rotationsSettings.TimeToRotateInSeconds;
+            _rotationsSettings.ObjectToRotate.rotation *= Quaternion.Euler(_angleModifier * Time.deltaTime * angularSpeedPerSeconds);
+            _timePassed += Time.deltaTime;
+            if (_timePassed > _timeBeforeStoppingInSeconds)
+            {
+                this.enabled = false;
+            }
+        }
+
+        void OnValidate()
+        {
+            CalculateModifierAccordingToReverse();
+        }
+
+        public override string ToString()
+        {
+            return $"{_identifier} makes a rotation with an angle of {_rotationsSettings.AngleRotation} every {_rotationsSettings.TimeToRotateInSeconds} seconds";
+        }
+
+        //Tout simplement des getteurs et des setteurs pour avoir acces et pouvoir modifier les attributs
+        public string getId()
+        {
+            return _identifier;
+        }
+        public void setId(string id)
+        {
+            this._identifier = id;
+        }
+        public float getTimeBeforeStopping()
+        {
+            return _timeBeforeStoppingInSeconds;
+        }
+        public void SetTimeBeforeStopping(float f)
+        {
+            this._timeBeforeStoppingInSeconds = f;
+        }
+        public bool getReverseRotation()
+        {
+            return _shouldReverseRotation;
+        }
+        public void SetReversePosition(bool b)
+        {
+            this._shouldReverseRotation = b;
+        }
+        public RotationSettings getRotationSettings()
+        {
+            return _rotationsSettings;
+        }
+        public void SetRotationSettings(RotationSettings rs)
+        {
+            this._rotationsSettings = rs;
+        }
+    }
+
+
+    //Pour avoir le bouton dans l'inspector qui ouvrira notre fenetre
+    [CustomEditor(typeof(Rotator))]
+    public class RotatorInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            if (GUILayout.Button("Open Rotator panel"))
+            {
+                RotatorEditor window = (RotatorEditor)EditorWindow.GetWindow(typeof(RotatorEditor), false, "Rotators Multiple Setter");
+                window.Show();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RotatorEditor.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using UnityEditor;
+namespace com.technical.test
+{
+    public class RotatorEditor : EditorWindow
+    {
+        //SelectedGO contiendra les gameObjects de notre tableau 
+        public GameObject[] SelectedGO;
+
+        //RotatorCopy stockera une copie du component Rotator, sachant que RotatorCopy[i] sera le component Rotator du gameObject SelectedGO[i]
+        public RotatorProperty[] RotatorCopy;
+
+        Vector2 scrollPosition = Vector2.zero;
+        private void Awake()
+        {
+            //On ajoute les objects selectionne dans l'inspector
+            GameObject[] tmp = new GameObject[256];
+            int i = 0;
+            foreach (GameObject go in Selection.gameObjects)
+            {
+                tmp[i++] = go;
+            }
+            SelectedGO = new GameObject[i];
+            RotatorCopy = new RotatorProperty[256];
+
+            for (int j = 0; j < i; j++)
+            {
+                SelectedGO[j] = tmp[j];
+                Rotator data = tmp[j].GetComponent<Rotator>();
+                if (data != null) RotatorCopy[j] = new RotatorProperty(tmp[j].GetComponent<Rotator>(), tmp[j].GetInstanceID());
+                else RotatorCopy[j] = null;
+            }
+        }
+
+        [MenuItem("Window/Custon/Rotators Multiple Setter")]
+        public static void ShowWindow()
+        {
+
+            GetWindow<RotatorEditor>("Rotators Multiple Setter");
+        }
+
+        void OnGUI()
+        {
+            //Pour la responsivite
+            scrollPosition = GUILayout.BeginScrollView(scrollPosition, true, true, GUILayout.Width(position.width), GUILayout.Height(position.height));
+            //Generation du tableau dans la fenetre
+            ScriptableObject scriptableObj = this;
+            SerializedObject serialObj = new SerializedObject(scriptableObj);
+            SerializedProperty serialProp = serialObj.FindProperty("SelectedGO");
+            EditorGUILayout.PropertyField(serialProp, true);
+
+            //True lorsque qu'on modifie le tableau
+            if (serialObj.hasModifiedProperties)
+            {
+                serialObj.ApplyModifiedProperties();
+                //On boucle sur notre nouveau tableau en modifiant RotatorCopy si il le faut
+                for (int i = 0; i < SelectedGO.Length; i++)
+                {
+                    if (SelectedGO[i] == null) RotatorCopy[i] = null;
+                    else
+                    {
+                        Rotator r = SelectedGO[i].GetComponent<Rotator>();
+                        if (r == null) RotatorCopy[i] = null;
+                        else if (RotatorCopy[i] == null) RotatorCopy[i] = new RotatorProperty(r, SelectedGO[i].GetInstanceID());
+                        else if (SelectedGO[i].GetInstanceID() != RotatorCopy[i].objectID)
+                            RotatorCopy[i] = new RotatorProperty(r, SelectedGO[i].GetInstanceID());
+                    }
+                }
+            }
+            reDraw();
+            GUILayout.EndScrollView();
+
+        }
+        //Lance DrawSettings sur tout les object dans SelectedGO
+        private void reDraw()
+        {
+            for (int i = 0; i < SelectedGO.Length; i++)
+            {
+                if (SelectedGO[i] != null) DrawSettings(i);
+            }
+        }
+
+        //Affiche le component tout en permettant les modifications
+        private void DrawSettings(int i)
+        {
+            GUILayout.Label(SelectedGO[i].name, EditorStyles.boldLabel);
+            RotatorProperty rp = RotatorCopy[i];
+            if (rp == null)
+            {
+                GUILayout.Label("Only GameObject who contain Rotator can be modify");
+                return;
+            }
+
+            rp.identifier = EditorGUILayout.TextField("Id", rp.identifier);
+            rp.timeBeforeStoppingInSeconds = EditorGUILayout.FloatField("Time Before Stopping (s)", rp.timeBeforeStoppingInSeconds);
+            rp.shouldReverseRotation = EditorGUILayout.Toggle("Reverse rotation", rp.shouldReverseRotation);
+            GUILayout.Label("Rotation settings");
+            rp.rotationsSettings.ObjectToRotate = EditorGUILayout.ObjectField("Object to rotate", rp.rotationsSettings.ObjectToRotate, typeof(Transform), true) as Transform;
+            rp.rotationsSettings.AngleRotation = EditorGUILayout.Vector3Field("Angle rotation", rp.rotationsSettings.AngleRotation);
+            rp.rotationsSettings.TimeToRotateInSeconds = EditorGUILayout.FloatField("Time to rotate", rp.rotationsSettings.TimeToRotateInSeconds);
+            if (GUILayout.Button("Save"))
+            {
+                rp.Paste(SelectedGO[i].GetComponent<Rotator>());
+            }
+        }
+    }
+
+    //Class permettant de stocker les données d'un component Rotator 
+    public class RotatorProperty
+    {
+        public string identifier;
+        public float timeBeforeStoppingInSeconds;
+        public bool shouldReverseRotation;
+        public RotationSettings rotationsSettings;
+        public int objectID;
+
+        public RotatorProperty(Rotator r, int id)
+        {
+            this.identifier = r.getId();
+            this.timeBeforeStoppingInSeconds = r.getTimeBeforeStopping();
+            this.shouldReverseRotation = r.getReverseRotation();
+            this.rotationsSettings = r.getRotationSettings();
+            this.objectID = id;
+        }
+
+        //Va se copier dans le component Rotator lui correspondant
+        public void Paste(Rotator data)
+        {
+            data.setId(this.identifier);
+            data.SetTimeBeforeStopping(this.timeBeforeStoppingInSeconds);
+            data.SetReversePosition(this.shouldReverseRotation);
+            data.SetRotationSettings(this.rotationsSettings);
+        }
+    }
+}
